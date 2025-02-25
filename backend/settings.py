2e0484ea5e00a876bc76f72d779bc4e940ee13b7
Etern1ty666 <novikov-reg@mail.ru>
2025-02-26 00:35:25 +0300

    hide django secret key

--- backend/settings.py
+++ backend/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_h#7yj$u)+!j!#lei=s^ydbu)v7p7%0q=vn0cxc&4t70t)p7)w'
+SECRET_KEY = str(os.getenv('DJANGO_SECRET_TOKEN'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
